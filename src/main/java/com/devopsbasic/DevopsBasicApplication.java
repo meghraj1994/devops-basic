969719b93784df4a03c32add6898ff5f2ceab740
meghrajkurmi <kurmimeghraj@gmail.com>
2022-05-29 17:05:55 -0500

    first commit

---
+++ src/main/java/com/devopsbasic/DevopsBasicApplication.java
@@ -0,0 +1,13 @@
+package com.devopsbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsBasicApplication.class, args);
+	}
+
+}
